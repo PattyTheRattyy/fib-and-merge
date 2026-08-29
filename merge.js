52c2764d5bcbdbfce83b0a9e7709b817c689df67
PatrickFavret <110774860+PatrickFavret@users.noreply.github.com>
2026-08-29 19:28:05 -0400

    CONSIDER IT MERGED

--- merge.js
+++ merge.js
@@ -1,8 +1,48 @@
 function mergeSort(arr) {
-  mid = Math.trunc(arr.length / 2);
-  left = arr.slice(0, mid);
-  right = arr.slice(mid);
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-  while (arr.length > 1) {}
-  mergeSort(arr);
+  const mid = Math.trunc(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  const leftArr = mergeSort(left);
+  const rightArr = mergeSort(right);
+  return merge(leftArr, rightArr);
 }
+
+function merge(leftArr, rightArr) {
+  let left = 0;
+  let right = 0;
+
+  let mergedArr = [];
+
+  while (left < leftArr.length && right < rightArr.length) {
+    if (leftArr[left] <= rightArr[right]) {
+      mergedArr.push(leftArr[left]);
+
+      left += 1;
+    } else {
+      mergedArr.push(rightArr[right]);
+      right += 1;
+    }
+  }
+
+  while (left < leftArr.length) {
+    mergedArr.push(leftArr[left]);
+    left += 1;
+  }
+  while (right < rightArr.length) {
+    mergedArr.push(rightArr[right]);
+    right += 1;
+  }
+  return mergedArr;
+}
+
+console.log(mergeSort([1, 3, 5, 3, 10, 2]));
+console.log(mergeSort([]));
+console.log(mergeSort([73]));
+console.log(mergeSort([1, 2, 3, 4, 5]));
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
